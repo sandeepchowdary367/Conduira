67072e5257b53f52daa6e4c192ded378717cad11
Lakshman Sandeep Gundala <66997198+sandeepchowdary367@users.noreply.github.com>
2022-03-14 21:44:05 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 class Main implements Runnable{  
 public void run(){  
-System.out.println("thread is running...");  
+System.out.println("Runnable thread");  
 }  
   
 public static void main(String args[]){  
-Main m1=new Main();  
-Thread t1 =new Thread(m1);
+Main s1=new Main();  
+Thread t1 =new Thread(s1);
 t1.start();  
  }  
 }  
